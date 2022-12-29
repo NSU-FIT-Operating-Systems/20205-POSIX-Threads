3e4a621bf23425c9dc8fc40950af9e60bb022635
mu2so4 <57590918+mu2so4@users.noreply.github.com>
2022-12-30 01:28:23 +0700

    muratov/17 (#25)
    * Lab17: Add Makefile
    
    * Lab17: Add list header file
    
    * Lab17: First version
    
    * Lab17: Apply fixes
    
    * Lab17: Fix buffer issue
    
    Устранена ошибка, из-за которой при вводе строки, равной размеру
    буфера, выводился список строк.
    
    Также устранена ошибка с потерей символов при обрезании строк,
    чья длина больше буфера.
    
    * Lab17: Fix dot issue
    
    Устранил баг, вызванный тем, что если в конец строки, чей размер
    кратен размеру буфера, дописать точку, то программа закроется.
    
    А это должно происходить, если вся строка -- точка.
    
    * Lab17: fix early lock issue
    
    А ещё я убрал поле end у List, потому что оно нигде не
    использовалось.
    
    * Lab17: Add instruction to exit program
    
    * Lab17: Fix sorting lock issue
    
    Я поправил блокировку потока при сортировке элемента. Теперь
    блокировка идёт непосредственно в момент обмена элементов списка.
    
    Также, чтобы не сортировать очищенные данные, я сначала отменяю
    поток сортировки, дожидаюсь его завершения, и только потом очищаю
    список. Mutex не является точкой канцелляции, так что сюрпризов
    при блокировке или разблокировке мьютексов быть не может. Тем
    не менее, я добавил дополнительную точку канцелляции внутри
    сортировки, чтобы можно было быстрее завершить поток, и при этом
    без нарушения транзакционности обмена элементов списка.

---
+++ muratov/lab17/list.c
@@ -0,0 +1,55 @@
+#include "list.h"
+
+int add_string(List *list, char *str) {
+	Item *item = malloc(sizeof(char) * strlen(str) + 1);
+	if(item == NULL) {
+		perror("malloc: failed to allocate memory for new string item");
+		return 0;
+	}
+	item->str = str;
+	pthread_mutex_lock(&list_mutex);
+	item->next = list->begin;
+	list->begin = item;
+	list->size++;
+	pthread_mutex_unlock(&list_mutex);
+	return 1;
+}
+
+
+void sort(List *list) {
+	for(int pos = 0; pos < list->size - 1; pos++) {
+		Item *item = list->begin;
+		for(int shift = pos; shift < list->size - 1; shift++) {
+			if(strcmp(item->str, item->next->str) > 0) {
+				pthread_mutex_lock(&list_mutex);
+				char *tmp = item->str;
+				item->str = item->next->str;
+				item->next->str = tmp;
+				pthread_mutex_unlock(&list_mutex);
+			}
+			pthread_testcancel();
+			item = item->next;
+		}
+	}
+}
+
+void print_list(const List *list) {
+	pthread_mutex_lock(&list_mutex);
+	for(Item *iter = list->begin; iter != NULL; iter = iter->next) {
+		printf("%s\n", iter->str);
+	}
+	pthread_mutex_unlock(&list_mutex);
+}
+
+void clear_list(List *list) {
+	pthread_mutex_lock(&list_mutex);
+	for(Item *iter = list->begin; iter != NULL;) {
+		Item *next = iter->next;
+		free(iter->str);
+		free(iter);
+		iter = next;
+	}
+	list->size = 0;
+	list->begin = NULL;
+	pthread_mutex_unlock(&list_mutex);
+}
---
+++ muratov/lab17/reader.c
@@ -0,0 +1,83 @@
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define SORT_DELAY 5
+#define MAX_LENGTH 80
+
+List list;
+pthread_mutex_t list_mutex;
+
+void *sort_thread() {
+	while(1) {
+		sleep(SORT_DELAY);
+		sort(&list);
+	}
+	pthread_exit(NULL);
+
+}
+
+void clear_all(pthread_t sorter) {
+	pthread_cancel(sorter);
+	if(pthread_join(sorter, NULL) != 0) {
+		perror("failed to join sorting thread");
+	}
+	clear_list(&list);
+	pthread_mutex_destroy(&list_mutex);
+}
+
+int main() {
+	printf("Enter line \".\" to exit\n\n");
+	pthread_t sorter;
+	list.begin = NULL;
+	list.size = 0;
+
+	if(pthread_mutex_init(&list_mutex, NULL) != 0) {
+		perror("failed to init mutex");
+		return 1;
+	}
+	if(pthread_create(&sorter, NULL, sort_thread, NULL) != 0) {
+		perror("failed to create thread");
+		pthread_mutex_destroy(&list_mutex);
+		return 1;
+	}
+	char buffer[MAX_LENGTH + 1];
+	char last_char = '\n';
+	while(1) {
+		ssize_t count = read(STDIN_FILENO, buffer, MAX_LENGTH);
+		if(count < 0) {
+			perror("error on reading");
+			clear_all(sorter);
+			return 1;
+		}
+		if((strncmp(".\n", buffer, 2) == 0 && last_char == '\n') || count == 0) {
+			break;
+		}
+		if(count == 1) {
+			if(last_char == '\n') {
+				print_list(&list);
+			}
+			last_char = '\n';
+			continue;
+		}
+		last_char = buffer[count - 1];
+		if(buffer[count - 1] == '\n') {
+			buffer[count - 1] = '\0';
+		}
+		char *copy = (char*) malloc(sizeof(char) * (count + 1));
+		if(copy == NULL) {
+			perror("failed to allocate memory for new string");
+			clear_all(sorter);
+			return 1;
+		}
+		strcpy(copy, buffer);
+		if(!add_string(&list, copy)) {
+			free(copy);
+			clear_all(sorter);
+			return 1;
+		}
+	}
+	clear_all(sorter);
+
+}
